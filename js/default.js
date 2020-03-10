c60fe21e0de0fdb27f202bc499ae189403523da8
ms-ghasemi <ms-ghasemi@agri-bank.com>
2020-03-10 12:48:04 +0330

    add team

--- js/default.js
+++ js/default.js
@@ -1,10 +1,26 @@
-$(function(){
-     new WOW().init();
+$(function () {
+    new WOW().init();
 });
 
-$(document).ready(function() {
-  $('.works-container').magnificPopup({delegate: 'a',type:'image', gallery: {
-    // options for gallery
-    enabled: true
-  }});
+$(document).ready(function () {
+    $('.works-container').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        gallery: {
+            // options for gallery
+            enabled: true
+        }
+    });
+});
+
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+         items :3,
+        loop: true,
+        margin: 20,
+        responsiveClass: true,
+        autoplay: true,
+       dotsEach:true,
+        center:true
+    });
 });
