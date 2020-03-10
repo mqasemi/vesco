c885c759b367b5f45d24c5114edb348c9ff4113a
ms-ghasemi <ms-ghasemi@agri-bank.com>
2020-03-10 10:25:57 +0330

    add work section

--- js/default.js
+++ js/default.js
@@ -1,3 +1,10 @@
 $(function(){
      new WOW().init();
 });
+
+$(document).ready(function() {
+  $('.works-container').magnificPopup({delegate: 'a',type:'image', gallery: {
+    // options for gallery
+    enabled: true
+  }});
+});
